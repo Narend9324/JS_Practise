e915f18b0a0066f2183260316835dbb67ed9284f
Narend9324 <nc379783@gmail.com>
2024-08-02 01:02:35 +0530

    Day 1 of 30DaysOfJsChallenge

---
+++ 30DaysJsChallenge/Day1.js
@@ -0,0 +1,46 @@
+//Activity 1  ** Variable declaration **
+//Task 1
+var number = 3;
+console.log("number", number);
+
+//Task 2
+let abc = "narend";
+console.log("abc", abc);
+
+//Activity 2   ** constant Declaration **
+//Task 3
+const isGujarat = false;
+console.log("isGujarat", isGujarat);
+
+//Activity 3  ** Data Type **
+//Task 4
+
+let name = "narend";
+let age = 25;
+let isWorking = true;
+let myDataObject = {
+  name: "narend",
+  last: "choudhary",
+  age: 24,
+  eyeColor: "blue",
+};
+let myDataArray = [1, 2, 3, 4, 2, "narend"];
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isWorking);
+console.log(typeof myDataObject);
+console.log(typeof myDataArray);
+
+//Activity 4   ** Reassiging variable **
+//Task 5
+
+let location = "mumbai";
+console.log("location0", location);
+location = "ahmedabad";
+console.log("location1", location);
+
+//Activity 5 ** Understanding const**
+const myName = "narendra";
+myName = "choudhary";
+console.log("myName", myName);
